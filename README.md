5c974a15fd1aecc612eed1db2936c655cca00c94
mahad09 <mahad.asif.git@gmail.com>
2021-12-07 00:43:13 +0500

    completed code.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Arabic-hand-gestures-classification
 Hand gesture recognition using CNNs. Using the dataset to train the best known network for gesture classification on images with just one hand. Testing hyper-parameters and preprocessing techniques to improve the performance.
+
+Steps to run the code:
+
+1)Place the dataset directory namely 'augmented_dataset' in the root directory of the project.
+2)Run the command 'python3 main.py' from the terminal window OR run the 'main.py' in any IDE to kick-start the code execution.
+
+NOTE: 'data_augmentation.py' is only executed when you don't have the augmented dataset. It is more important to note that the names of dataset directories or folders must match with the names defined in the code. 
+
+Thank.
+
---
+++ data_augmentation.py
@@ -0,0 +1,52 @@
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import cv2
+import os
+
+
+DATASET_PATH = 'full_dataset/'
+
+datagen = ImageDataGenerator(
+  width_shift_range=0.2,
+  height_shift_range=0.2,
+  brightness_range=[0.3,1.7],
+  zoom_range=[0.8,1.2]
+  )
+
+# # generate samples and plot
+# for i in range(5):
+#   # define subplot
+#   plt.subplot(330 + 1 + i)
+#   # generate batch of images
+#   batch = it.next()
+#   # convert to unsigned integers for viewing
+#   image = batch[0].astype('uint8')
+#   # plot raw pixel data
+#   plt.imshow(image)
+# # show the figure
+# plt.show()
+
+new_folder = 'augmented_dataset'
+if not (os.path.isdir(new_folder)): os.mkdir(new_folder) 
+if not os.listdir(new_folder):
+  [os.mkdir(new_folder+'/'+class_name) for class_name in os.listdir(DATASET_PATH)]
+
+print('generating 9 augmented images from each original image ...')
+
+for class_name in tqdm(os.listdir(DATASET_PATH)):
+  image_number = 1
+  for image_name in os.listdir(DATASET_PATH + class_name):    
+    img = load_img(DATASET_PATH + class_name + '/' + image_name)
+    data = img_to_array(img)
+    samples = expand_dims(data, 0)
+    it = datagen.flow(samples, batch_size=1)
+
+    for i in range(9):
+      batch = it.next()
+      image = batch[0].astype('uint8')
+      cv2.imwrite(new_folder+ '/'+class_name+'/'+class_name+'_'+str(image_number)+'_aug_'+str(i)+'.jpg', image)
+      image_number += 1
